d6f106ca4a80e022f044da139669d709dbf3f4ca
Sebastian Pape <email@sebastianpape.com>
2013-08-21 21:24:28 +0200

    first draft

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-haml ./example/src/example.haml ./example/example.html && sass ./src/css/ambilight-table.sass ./lib/css/ambilight-table.css && coffee --compile --output ./lib/js/ ./src/js/ambilight-table.coffee && sass ./example/src/example.sass ./example/example.css
+haml ./example/src/example.haml ./example/example.html && sass ./src/css/ambilight-table.sass ./lib/css/ambilight-table.css && coffee --output ./lib/js/ --compile ./src/js/ambilight-table.coffee && cat ./vendor/StackBlur.js >> ./lib/js/ambilight-table.js && sass ./example/src/example.sass ./example/example.css
 exit
